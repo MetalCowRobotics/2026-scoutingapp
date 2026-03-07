fc7c5b9fc6a14c0e952ca742516afdd191547120
JAI-THE-GUY <146490870+JAI2011@users.noreply.github.com>
2026-03-06 23:04:23 -0600

    Fixed All Lint and Build Errors now ready for Deployment on vercel

--- next.config.ts
+++ next.config.ts
@@ -2,47 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  async redirects() {
-    return [
-      {
-        source: '/(.*)',
-        has: [
-          {
-            type: 'header',
-            key: 'x-forwarded-proto',
-            value: 'http',
-          },
-        ],
-        destination: 'host/:path*',
-        permanent: true,
-      },
-    ]
-  },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Strict-Transport-Security',
-            value: 'max-age=31536000; includeSubDomains',
-          },
-          {
-            key: 'X-Frame-Options',
-            value: 'DENY',
-          },
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-          {
-            key: 'Referrer-Policy',
-            value: 'origin-when-cross-origin',
-          },
-        ],
-      },
-    ]
-  },
 };
 
 export default nextConfig;
